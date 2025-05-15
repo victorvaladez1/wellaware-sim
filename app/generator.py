b2f609def5accdcc8dc41a9f855254ae7044d3d2
Victor Valadez <vvaladez315@gmail.com>
2025-05-15 10:19:47 -0500

    Add anomaly status to sensor readings: HIGH PRESSURE and LOW FLOW

--- app/generator.py
+++ app/generator.py
@@ -4,16 +4,26 @@ import json
 import os
 
 def generate_sensor_reading(well_id):
+    pressure = round(random.uniform(100, 250), 2)
+    temperature = round(random.uniform(70, 120), 2)
+    flow_rate = round(random.uniform(20, 50), 2)
+
+    status = "NORMAL"
+    if pressure > 230:
+        status = "HIGH PRESSURE"
+    elif flow_rate < 25:
+        status = "LOW FLOW"
+
     return {
         "well_id": well_id,
         "timestamp": datetime.datetime.utcnow().isoformat() + "Z",
-        "pressure": round(random.uniform(100, 250), 2),     #psi
-        "temperature": round(random.uniform(70, 120), 2),   # F
-        "flow_rate": round(random.uniform(20, 50), 2),      # barrels/hours
+        "pressure": pressure,
+        "temperature": temperature,
+        "flow_rate": flow_rate,
+        "status": status
     }
 
 def get_all_wells():
     json_path = os.path.join(os.path.dirname(__file__), 'wells.json')
     with open(json_path, 'r') as file:
-        wells = json.load(file)
-    return wells
+        return json.load(file)
